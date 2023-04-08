bedfa7764e35d2941961cf9ac4fd015c50d3e173
Sharmin64 <lizaeatsbanana@gmail.com>
2023-04-08 16:56:11 +0600

    file added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,10 +7,10 @@ const Cart = ({cart}) => {
   let totalShipping = 0
   let quantity=0
   for (const product of cart) {
-    //if (product.quantity = 0) {
-    //  product.quantity=1
-    //}
-    //product.quantity=product.quantity||1
+    if (product.quantity = 0) {
+      product.quantity=1
+    }
+    product.quantity=product.quantity||1
     totalPrice = totalPrice + product.price*product.quantity
     totalShipping = totalShipping + product.shipping
     quantity=quantity+product.quantity
@@ -20,7 +20,7 @@ const Cart = ({cart}) => {
   return (
     <div className='cart'>
         <h2>Order Summery</h2>
-      <p>Selected Items:{cart.length}</p>
+      <p>Selected Items:{quantity}</p>
       <p>Total price: $ {totalPrice}</p>
       <p>Total Shipping Cost: ${totalShipping}</p>
       <p>Tax:$ {tax.toFixed(2)}</p>
